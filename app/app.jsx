dc4d2adfdc5df40d3887e7208bf9153b10844871
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-03-12 22:26:07 -0600

    dd

---
+++ app/app.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var objOne = {
+  name: 'George',
+  location: 'Provo'
+};
+var objTwo = {
+  age: 22,
+  ...objOne
+};
+console.log(objTwo);
+
+ReactDOM.render(
+  <h1>boilerplate app!</h1>,
+  document.getElementById('app')
+);
